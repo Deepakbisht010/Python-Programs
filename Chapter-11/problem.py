4a012af86b044f295cd0ea4e42d8a73d037c71dd
deepakbisht010 <deepakbisht4050@gmail.com>
2026-04-06 22:45:38 +0530

    New Commit

--- Chapter-11/problem.py
+++ Chapter-11/problem.py
@@ -26,3 +26,60 @@ b=width
 rec=Rectangle(a,b)
 rec.display() #Rectangle.display(rec)
 
+
+
+# 📝Create a BankAccount class.
+# The constructor should take account_holder_name and balance.
+# Create methods:
+
+# deposit(amount)
+# withdraw(amount)
+# show_balance()
+
+class BankAccount:
+    def __init__(slef,ac_holder_name,balance):
+        slef.ac_holder_name=ac_holder_name
+        slef.balance=balance
+    
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        return self.balance
+    
+    def withdraw(self,amount):
+        self.balance=self.balance-amount
+        return self.balance
+    
+    def show_balance(self):
+        print(f"The Accound Holder Name is : {self.ac_holder_name} \n")
+        print(f"The balance is : {self.balance}")
+        print("")
+        
+bank=BankAccount("Deepak",10000)
+bank.show_balance()
+
+bank.deposit(1000)
+bank.show_balance()
+
+bank.withdraw(500)
+bank.show_balance()
+
+
+
+# 📝Create a Circle class.
+# The constructor should take radius.
+# Create a method to calculate and display the area of the circle (π = 3.14).
+
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
+      
+    def display(self):
+        PI=3.14
+        return PI*(self.radius*self.radius)
+
+
+radius=float(input("Enter the radius of the circle : "))
+  
+are_cle=Circle(radius)
+area_of_circle=are_cle.display() #Circle.display(are_cle)
+print(f"The area of the circle is : {area_of_circle}")
